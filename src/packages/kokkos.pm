b1409d251476e25d3ee935bf010fce2a8af2acc8
landinjm <landinjm@umich.edu>
2025-06-09 13:44:35 -0400

    kokkos package

--- src/packages/kokkos.pm
+++ src/packages/kokkos.pm
@@ -5,17 +5,63 @@ use warnings;
 
 use lib 'src';
 use utilities;
+use archive_manager;
+use File::Path qw(rmtree);
 
 our $PRIORITY = 8;
 
-sub fetch { }
+our $VERSION      = "4.6.01";
+our $NAME         = "kokkos";
+our $SOURCE_URL   = "https://github.com/kokkos/kokkos/releases/download/";
+our $PACKING_TYPE = "tar.gz";
+our $CHECKSUM =
+  "b9d70e4653b87a06dbb48d63291bf248058c7c7db4bd91979676ad5609bb1a3a";
+
+sub fetch {
+
+    # Construct the archive url
+    my $archive_url = "$SOURCE_URL$VERSION/$NAME-$VERSION.$PACKING_TYPE";
+
+    # Download the archive
+    archive_manager::download_archive( $archive_url, $CHECKSUM );
+
+}
 
 sub unpack {
     my $unpack_path = shift;
+
+    # Double check that the archive was downloaded
+    if ( !utilities::file_exists("$NAME-$VERSION.$PACKING_TYPE") ) {
+        utilities::color_print(
+            "Error: Archive $NAME-$VERSION.$PACKING_TYPE not found", "bad" );
+        exit 1;
+    }
+
+    # Remove the old unpacked directory
+    if ( utilities::directory_exists("$unpack_path/$NAME-$VERSION") ) {
+        rmtree("$unpack_path/$NAME-$VERSION");
+    }
+
+    # Unpack the archive
+    system("tar -xzf $NAME-$VERSION.$PACKING_TYPE -C $unpack_path");
 }
 
 sub build {
     my ( $unpack_path, $install_path ) = @_;
+
+    # Create the build folder
+    mkdir("$NAME-$VERSION");
+
+    # Navigate to the build folder
+    chdir("$NAME-$VERSION");
+
+    # Run cmake
+    system(
+"cmake -DCMAKE_INSTALL_PREFIX=$install_path/$NAME-$VERSION $unpack_path/$NAME-$VERSION"
+    );
+
+    # Build
+    system("make && make install");
 }
 
 sub register {
